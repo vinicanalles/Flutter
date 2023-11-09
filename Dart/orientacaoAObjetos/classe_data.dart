a6d1251d8ed6cd9e8570a6bf258eab6ff7484103
vinicanalles <62850892+vinicanalles@users.noreply.github.com>
2023-11-08 22:46:55 -0300

    feat: aprendendo sobre construtores
    - Aprendendo a utilizar construtores nas classes da linguagem Dart;

--- Dart/orientacaoAObjetos/classe_data.dart
+++ Dart/orientacaoAObjetos/classe_data.dart
@@ -3,6 +3,14 @@ class Data {
   int? mes;
   int? ano;
 
+  // Data(int dia, int mes, int ano) {
+  //   this.dia = dia;
+  //   this.mes = mes;
+  //   this.ano = ano;
+  // }
+
+  Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
+
   String obterFormatada() {
     return "$dia/$mes/$ano";
   }
@@ -13,18 +21,16 @@ class Data {
 }
 
 main() {
-  var dataAniversario = new Data();
-  dataAniversario.dia = 3;
-  dataAniversario.mes = 10;
-  dataAniversario.ano = 2020;
+  var dataAniversario = new Data(3, 10, 2020);
+  // dataAniversario.dia = 3;
+  // dataAniversario.mes = 10;
+  // dataAniversario.ano = 2020;
 
-  Data dataCompra = Data();
-  dataCompra.dia = 23;
+  Data dataCompra = Data(1, 1, 1970);
+  // dataCompra.dia = 23;
   dataCompra.mes = 12;
   dataCompra.ano = 2021;
 
-  // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
-  // print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
   String d1 = dataAniversario.obterFormatada();
 
   print("A data do aniversário é $d1");
@@ -32,4 +38,9 @@ main() {
 
   print(dataCompra);
   print(dataCompra.toString());
+
+  print(new Data());
+  print(Data(31));
+  print(Data(31, 12));
+  print(Data(31, 12, 2021));
 }
