0050d9c79f7c617081bc84d4640c66a1637a36b8
vinicanalles <62850892+vinicanalles@users.noreply.github.com>
2023-11-08 22:44:44 -0300

    feat: aprendendo sobre métodos
    - Aprendendo a utilizar métodos dentro das classes na linguagem Dart;

--- Dart/orientacaoAObjetos/classe_data.dart
+++ Dart/orientacaoAObjetos/classe_data.dart
@@ -2,6 +2,14 @@ class Data {
   int? dia;
   int? mes;
   int? ano;
+
+  String obterFormatada() {
+    return "$dia/$mes/$ano";
+  }
+
+  String toString() {
+    return obterFormatada();
+  }
 }
 
 main() {
@@ -15,6 +23,13 @@ main() {
   dataCompra.mes = 12;
   dataCompra.ano = 2021;
 
-  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
-  print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+  // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  // print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+  String d1 = dataAniversario.obterFormatada();
+
+  print("A data do aniversário é $d1");
+  print("A data da compra é ${dataCompra.obterFormatada()}");
+
+  print(dataCompra);
+  print(dataCompra.toString());
 }
